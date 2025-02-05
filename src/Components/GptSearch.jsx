aa3ad47ba731b6e2757d96bc03a4aa6d088eeaf6
Tushar Batra <tusharbatra08@gmail.com>
2025-02-05 19:36:14 +0530

    One more error resolved

--- src/Components/GptSearch.jsx
+++ src/Components/GptSearch.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import GptSearchBar from "./GptSearchBar";
-import GptMovieSuggestions from "./gptMovieSuggestions";
+import GptMovieSuggestions from "./GptMovieSuggestions";
 import { BG_URL } from "../utils/constants";
 
 const GptSearch = () => {
