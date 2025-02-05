c0f3a1834ddd2029bd7ab3a8fb5b7f3aa4737ba5
Tushar Batra <tusharbatra08@gmail.com>
2025-02-05 19:10:29 +0530

    Handled deployment error

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import moviesReducer from "./movieSlice";
-import gptReducer from "./GptSlice";
+import gptReducer from "./gptSlice";
 import configReducer from "./configSlice";
 
 const appStore = configureStore({
